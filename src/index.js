1e11eca09262e67e0d95a84f0f19af1915725d8e
NicoleScott-web <nicolemilyscott@gmail.com>
2024-12-09 18:26:13 +1300

    adding dotenv

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Is this working?')
+  res.send(`Hello World! Is this working?${process.env.HOST_ENV} this is my secret value ${process.env.SECRET} `)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${process.env.HOST_ENV}`)
 })
 // docker build -t nicolescott/mvc-structure .
